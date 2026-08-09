40fc45ce7ba74242a27e36df1efc20e356d07da7
pmventures-aa <c.raymondjenkins@gmail.com>
2026-08-09 16:50:50 -0400

    Add service assignment workspace route

--- src/pages/admin/AdminApp.tsx
+++ src/pages/admin/AdminApp.tsx
@@ -26,8 +26,9 @@ import EmployeesAdmin from './EmployeesAdmin'
 import ReportingCenter from './ReportingCenter'
 import CommunicationsCRMAdmin from './CommunicationsCRMAdmin'
 import InvoicesAdmin from './InvoicesAdmin'
+import ServiceAssignmentsAdmin from './ServiceAssignmentsAdmin'
 
-const STAFF_VISIBLE = ['dashboard', 'pipelines', 'clients', 'inquiries', 'messages', 'activity', 'invoices']
+const STAFF_VISIBLE = ['dashboard', 'pipelines', 'clients', 'inquiries', 'messages', 'activity', 'service-assignments', 'invoices']
 
 function AdminShell() {
   const { user } = useAuth()
@@ -70,6 +71,7 @@ export default function AdminApp({ basePath }: { basePath: string }) {
             <Route path="leads/:id" element={<LeadDetail />} />
             <Route path="messages" element={<MessagesAdmin />} />
             <Route path="activity" element={<ActivityAdmin />} />
+            <Route path="service-assignments" element={<ServiceAssignmentsAdmin />} />
             <Route path="invoices" element={<InvoicesAdmin />} />
             <Route path="audit-log" element={<AuditLogAdmin />} />
             <Route path="reports" element={<ReportingCenter />} />
