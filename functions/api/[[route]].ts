a63474316ea2a00090bdd146772c59fa4691c6e7
pmventures-aa <c.raymondjenkins@gmail.com>
2026-08-09 14:47:55 -0400

    Mount CRM record routes

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -17,6 +17,7 @@ import { employeeRoutes } from '../_lib/routes/employees'
 import { reportRoutes } from '../_lib/routes/reports'
 import { searchRoutes } from '../_lib/routes/search'
 import { commsRoutes } from '../_lib/routes/comms'
+import { crmRoutes } from '../_lib/routes/crm'
 import { serviceApplicationRoutes } from '../_lib/routes/serviceApplications'
 import { intakeCatalogAdminRoutes } from '../_lib/routes/intakeCatalogAdmin'
 import { intakeCopyRoutes } from '../_lib/routes/intakeCopy'
@@ -44,6 +45,7 @@ app.route('/admin', employeeRoutes)
 app.route('/admin', reportRoutes)
 app.route('/admin', searchRoutes)
 app.route('/admin', commsRoutes)
+app.route('/admin', crmRoutes)
 
 app.get('/health', (c) => c.json({ ok: true, service: 'pmv-api', time: new Date().toISOString() }))
 
